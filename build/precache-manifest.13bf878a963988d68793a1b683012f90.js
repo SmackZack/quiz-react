34ba6ff841e9a3ef5e6851db128bbc3d73243c79
SmackZack <ashishone9781@gmail.com>
2020-12-14 11:41:42 +0530

    dsd

---
+++ build/precache-manifest.13bf878a963988d68793a1b683012f90.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47823b26d54c03ed74a3dd33f3c1f83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1945788fd01a967e036d",
+    "url": "/static/css/2.aa2de846.chunk.css"
+  },
+  {
+    "revision": "d26dc0f76fe741e88ef9",
+    "url": "/static/css/main.e13b1ab6.chunk.css"
+  },
+  {
+    "revision": "1945788fd01a967e036d",
+    "url": "/static/js/2.5ce92a81.chunk.js"
+  },
+  {
+    "revision": "d26dc0f76fe741e88ef9",
+    "url": "/static/js/main.56f93dcd.chunk.js"
+  },
+  {
+    "revision": "b223d76f0099d999956b",
+    "url": "/static/js/runtime-main.c42f9113.js"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/static/media/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/static/media/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/static/media/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/static/media/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
